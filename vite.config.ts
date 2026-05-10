f681574df4dd4d13fb4c160e689743ba0daeab1f
wira113 <ga.wira82@gmail.com>
2026-05-10 14:52:04 +0800

    git commit -m "add deploy workflow"

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/website-photographer-frontend/',  // nama repo kamu
-  plugins: [react(), tailwindcss(), tsconfigPaths() ],
+  base: process.env.NODE_ENV === 'production' ? '/website-photographer-frontend/' : '/',
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
